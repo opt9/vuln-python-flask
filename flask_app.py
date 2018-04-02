f7d01f268cd15523dd44ed035ddbf9f2bd3d912f
Jeremy Bae <jswbae@gmail.com>
2018-04-02 23:45:06 +0900

    Fix XSS vulnerabilities to remove confuse

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template_string, render_template
-
+from flask import Flask, request, render_template_string, render_template, Markup
 app = Flask(__name__)
 #app.debug = True
 
@@ -8,31 +7,33 @@ def hello_world():
     user = {'username':"world", 'secret':"dG9wIHNlY3JldA=="}
     if request.args.get('username'):
         user['username'] = request.args.get('username')
-    template = '''<h2>Hi %s!</h2>''' % user['username']
+    template = Markup('''<h2>Hi %s!</h2>''') % user['username']
     return render_template_string(template, user=user)
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-    template = '''{%% extends "layout.html" %%}
+    template = Markup(
+'''{%% extends "layout.html" %%}
 {%% block content %%}
     <div class="center-content error">
         <h1>Oops! That page doesn't exist.</h1>
         <h3>%s</h3>
     </div>
 {%% endblock %%}
-''' % (request.url)
+''') % (request.url)
     return render_template_string(template), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    template = '''{%% extends "layout.html" %%}
+    template = Markup(
+'''{%% extends "layout.html" %%}
 {%% block content %%}
     <div class="center-content error">
         <h1>Oops! Something wrong!</h1>
         <h3>%s</h3>
     </div>
 {%% endblock %%}
-''' % (request.url)
+''') % (request.url)
     return render_template_string(template), 500
